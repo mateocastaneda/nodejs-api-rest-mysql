516f957a49cdc90cfeaed7185bf1a38517b5ea5b
Mateo Castañeda <joanmateo27@gmail.com>
2021-05-21 19:33:17 -0500

    API REST en Node.js y Express.js

--- app/controllers/usuarios/usuarios.controller.js
+++ app/controllers/usuarios/usuarios.controller.js
@@ -7,7 +7,7 @@ exports.findAll = (req, res) => {
         message: err.message || "Some error occurred while retrieving usuarios."
       });
     } else {
-      res.send(data);
+      res.send(JSON.parse(data));
     }
   });
 };
